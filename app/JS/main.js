e1ca09a8b5dceec025ffd55067829c153fe0a0f6
eds-teh <frontendrazrabotka@gmail.com>
2022-06-02 01:25:21 +0300

    addet mixitup rateyo section, slick slider section, tutorial section

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
-    
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+    var mixer = mixitup('.products__inner-box');
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
 });
